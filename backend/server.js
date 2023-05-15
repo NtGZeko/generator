6cfe62eebb171a6117df7f4833a9d39f73c98c00
NtGZeko <matheooz@live.fr>
2023-05-15 06:40:02 +0200

    connection to databases okey

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv').config();
 const port = 5000;
 
+// connectDB
+connectDB();
+
 const app = express();
-
-// Connect to database
-require('dotenv').config();
-
-// database
-//   .getConnection()
-//   .then(() => {
-//     console.log('Can reach database');
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
 
 // Middleware
 app.use(express.json());

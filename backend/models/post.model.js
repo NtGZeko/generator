44607105750c626cf9dcbefb7c7de74969ec759d
NtGZeko <matheooz@live.fr>
2023-05-15 07:06:19 +0200

    finish models

---
+++ backend/models/post.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+  excuse: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('post', postSchema);

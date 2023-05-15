ad37be48ba13390d756dcdbb3179a1a68a0f72c5
NtGZeko <matheooz@live.fr>
2023-05-15 20:35:00 +0200

    add dependencies for CORS and finish first call databases is OK

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const { getPosts } = require('./controllers/post.controllers');
 const dotenv = require('dotenv').config();
 const port = 5000;
+const cors = require('cors');
 
 // connectDB
 connectDB();
@@ -11,8 +13,9 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cors());
 app.use('/post', require('./routes/post.routes'));
 
 // Lunch the server
+
 app.listen(port, () => console.log('Server is running on port ' + port));

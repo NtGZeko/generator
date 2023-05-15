576c271bebc1320c8e2aec38316056c0897d6cfa
NtGZeko <matheooz@live.fr>
2023-05-15 09:01:13 +0200

    finish controllers

--- backend/server.js
+++ backend/server.js
@@ -13,5 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/post', require('./routes/post.routes'));
+
 // Lunch the server
 app.listen(port, () => console.log('Server is running on port ' + port));

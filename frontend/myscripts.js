7a142f4c319101fef588e3be1cd53a3e79d9308b
NtGZeko <matheooz@live.fr>
2023-05-16 12:54:49 +0200

    fetch backend OK! , and random button

--- frontend/myscripts.js
+++ frontend/myscripts.js
@@ -1,3 +1,18 @@
-fetch('http://localhost:5000/post')
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// fetch(`http://localhost:5000/post`)
+//   .then((res) => res.json())
+//   .then((data) => console.log(data[6].excuse));
+
+fetch(`http://localhost:5000/post`).then((res) =>
+  res.json().then((data) => {
+    console.log(data);
+    document.querySelector('#excuse').innerHTML =
+      data[Math.floor(Math.random() * data.length)].excuse;
+  })
+);
+
+const refreshButton = document.querySelector('#refresh-button');
+const refreshPage = () => {
+  location.reload();
+};
+
+refreshButton.addEventListener('click', refreshPage);
